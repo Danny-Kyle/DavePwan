81531589e776def666f879af15976051a050e290
Okechukwu Daniel <dikedaniel7917@gmail.com>
2025-07-07 07:25:29 +0100

    fixed the propertiesPropCard

--- src/features/PropPropertiesCard.js
+++ src/features/PropPropertiesCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PropPropertiesCard = ({title, location}) => {
   return (
-    <div className="bg-white rounded shadow p-4 w-[22rem] h-32 flex flex-col justify-center">
+    <div className="bg-white rounded shadow p-3 md:p-4 max-w-xs sm:max-w-sm md:w-[22rem] w-full h-auto min-h-32 flex flex-col justify-center">
       <h3 className="font-bold text-gray-800">{title}</h3>
       <p className="text-gray-600">{location}</p>
       <a className="text-red-600 font-semibold mt-2" href="#">
